dfa61235672efe6b4e13a370b10432842eb22dcd
Zsolt HORVATH <h.zsola@gmail.com>
2021-07-30 12:04:57 +0200

    Sign Up fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,50 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const userModel = require("../models/User");
 const router = new express.Router();
+const SALT=9;
+
+
+router.post("/signup", async (req, res, next) => {
+    
+    try{
+         
+        const user = req.body;
+        
+        if(!user.lastname || !user.firstname || !user.password || !user.email) {
+            console.log("I am here");
+            res.render('signup.hbs');
+            return;
+        }
+
+        const foundUser = await userModel.findOne({email:user.email});
+        
+        if (foundUser){
+            res.render('signup.hbs', {msg:{status:"ERROR:", text:"This e-mail is already used. Please choose another one"}});
+            return;
+        }
+
+        //If it is a new new user encrypt the pswd and give it to the user
+        const hashedPassword = bcrypt.hashSync(user.password, SALT);
+        user.password=hashedPassword;
+        
+        //Inject the new user to our database
+        userModel.create(user)
+            .then((createdUser)=>{
+                console.log("new User:");
+                console.log(createdUser);
+                res.redirect('/signin');})
+            .catch((error)=>{console.log(error)})
+
+    }
+    catch (error) {next(error);}
+
+    //res.render("signup");
+  });
+
+
+
+
+
 module.exports = router;
